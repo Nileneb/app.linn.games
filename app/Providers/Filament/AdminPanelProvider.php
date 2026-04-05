f96293f5bc79438b417d61a60c44bbed86315bd6
nileneb <bene.linn@yahoo.de>
2026-04-05 18:07:11 +0200

    feat: update admin access control in dashboard view and remove default panel setting

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -21,7 +21,6 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
             ->id('admin')
             ->path('admin')
             ->login()
